82a656eba143a1f5e93072f5df2c9ea3387eaa97
Jongbum Won <71166964+Wondaeng@users.noreply.github.com>
2025-09-22 10:24:47 +0900

    Update example1.c

--- data_structure/lecture3/example1.c
+++ data_structure/lecture3/example1.c
@@ -9,9 +9,26 @@ int main() {
         // scanf, gets --> 배열 사이즈를 초과해도 주어진 메모리를 넘어 값을 씀 (오버플로우)
         // stdin: 표준 입력 파일에 대한 포인터, fgets는 stdin뿐아닌 파일 읽어옴
         // fgets가 줄바꿈 문자"까지" 버퍼에 저장함
+        
+        /*
         fgets(buffer, BUFFER_SIZE, stdin); // line 단위 입력 (newline 기준)
         buffer[strlen(buffer) - 1] = "\0";
+        */
+        read_line(buffer, BUFFER_SIZE);
         printf("%s:%d\n", buffer, strlen(buffer));
     }
     return 0;
+}
+
+int read_line( char str[], int limit) {
+    int ch, i = 0;
+    
+    while ((ch = getchar()) != '\n') {
+        if (i < limit) {
+            str[i++] = ch;
+        }
+    }
+    
+    str[i] = '\0';
+    return i;
 }
