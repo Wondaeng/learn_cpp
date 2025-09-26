a7860129c1ecee6688c0cfa9ae162de80af0ddd0
Jongbum Won <71166964+Wondaeng@users.noreply.github.com>
2025-09-26 10:26:48 +0900

    Create strtok.c

---
+++ data_structure/lecture8/strtok.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[] = "now # is the time # to start preparing ### for the exam#";
+    char delim[] = "#";
+    char* token;
+    
+    /*
+    first call: strtok returns pointer of the start of the first token
+    e.g., strtok(str, delim);
+    next call: pointer of the start of following tokens
+    e.g., strtok(NULL, delim);
+    NOTE: strtok actually replace delimeter with '\0' null character
+    */
+    token = strtok(str, delim);
+    
+    while (token != NULL) {
+        printf("next token is: %s:%d\n", token, strlen(token));
+        token = strtok(NULL, delim);
+    }
+    return 0;
+}
