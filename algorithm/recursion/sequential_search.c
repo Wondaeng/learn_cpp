ccb4b118e81cd636bb8faaf30d5b5d60e81787ba
Jongbum Won <71166964+Wondaeng@users.noreply.github.com>
2025-11-06 13:45:28 +0900

    Create sequential_search.c

---
+++ algorithm/recursion/sequential_search.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// sequential search
+int search1(int data[], int n, int target) {
+    for (int i = 0; i < n; i++) {  // 인덱스를 0부터 n-1까지 순회
+        if (data[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+// recursive version
+int search2(int data[], int begin, int end, int target) {
+    if (begin > end) {
+        return -1;
+    }
+    else if (target == items[begin]) {
+        return begin;
+    }
+    else {
+        return search2(data, begin + 1, end, target);
+    }
+}
