a97bd14abefc46e33aae79a605c188cf85155e83
Jongbum Won <71166964+Wondaeng@users.noreply.github.com>
2025-11-17 15:54:49 +0900

    Update my_strings.h

--- week8/ft_printf/my_strings.h
+++ week8/ft_printf/my_strings.h
@@ -2,11 +2,16 @@
 #ifndef MY_STRINGS_H
 #define MY_STRINGS_H
 
+#include <stdlib.h>
+#include <assert.h>
+
 size_t strlen(char* str);
 char* strdup(char* str);
 char* strjoin(char* str1, char* str2);
 char* strchr(const char*, int c);
 char* strncpy(char* dest, const char* src, size_t n);
 char* ptr_to_hex(void* ptr);
+char* int_to_str(int number);
+char* uint_to_str(unsigned int number);
 
 #endif
