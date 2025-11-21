110437ec980d2b89eb41029684d09eb05c86a411
Jongbum Won <71166964+Wondaeng@users.noreply.github.com>
2025-11-21 14:00:30 +0900

    Create n_and_m_8.c
    https://www.acmicpc.net/problem/15657

---
+++ baekjoon/recursive/n_and_m_8.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int compare(const void* a, const void* b) {
+    int x = *(int*)a;
+    int y = *(int*)b;
+    return x - y;
+}
+
+
+void print_sequence(int depth, int start, int* arr, int* result, int n, int m) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            printf("%d ", result[i]);
+        }
+        printf("\n");
+        return;
+    }
+        
+    for (int i = start; i < n; i++) {
+    
+        result[depth] = arr[i];
+        print_sequence(depth + 1, i, arr, result, n, m);
+    }
+}
+
+
+int main() {
+    int n = 0;
+    int m = 0;
+    
+    scanf("%d %d", &n, &m);
+    
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);      
+    }    
+	
+	qsort(arr, n, sizeof(int), compare);
+    
+    int* result = (int*)malloc(m * sizeof(int));
+    
+    print_sequence(0, 0, arr, result, n, m);
+    
+    free(arr);
+    free(result);
+    
+    return 0;   
+}
+
