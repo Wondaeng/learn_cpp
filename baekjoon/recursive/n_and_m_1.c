411bdf42730566ae31ab8595781d254cf0771393
Jongbum Won <71166964+Wondaeng@users.noreply.github.com>
2025-11-24 15:45:57 +0900

    Create n_and_m_1.c
    https://www.acmicpc.net/problem/15649

---
+++ baekjoon/recursive/n_and_m_1.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_sequence(int index, int depth, int n, int* arr, int* result, int* used) {
+	if (index == depth) {
+		for (int i = 0; i < depth; i++) {
+			printf("%d ", result[i]);
+		}
+		printf("\n");
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			if (used[i]) {
+				continue;
+			}
+
+			used[i] = 1;
+			result[index] = arr[i];
+			print_sequence(index + 1, depth, n, arr, result, used);
+			used[i] = 0;
+		}
+	}
+}
+
+
+int main() {
+	int arr[8] = { 0 };
+	
+	int n = 0;
+	int m = 0;
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+	}
+
+	int* result = (int*)malloc(m * sizeof(int));
+	int* used = (int*)calloc(n, sizeof(int));
+
+	print_sequence(0, m, n, arr, result, used);
+
+	free(result);
+	free(used);
+
+	return 0;
+}
